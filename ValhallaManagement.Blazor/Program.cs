dc28d86848d2605de827d68b43212ac863bb5733
Elkin Londoño Díaz <Elkinlon0930@hotmail.com>
2024-09-24 00:56:13 -0500

    Se crear archivos de arranque y configuracion

---
+++ ValhallaManagement.Blazor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ValhallaManagement.Blazor;
+using ValhallaManagement.Blazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Registrar el servicio de Vikingo
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IVikingoService, VikingoService>();
+
+await builder.Build().RunAsync();
